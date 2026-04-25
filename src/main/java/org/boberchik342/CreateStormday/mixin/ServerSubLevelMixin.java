7d9576a1344dfd34796752b14e4f9ee442fe15da
boberchik342 <Mikhail.Kamyshev.10@gmail.com>
2026-04-25 17:16:23 -0400

    fixed objects sleeping

--- src/main/java/org/boberchik342/CreateStormday/mixin/ServerSubLevelMixin.java
+++ src/main/java/org/boberchik342/CreateStormday/mixin/ServerSubLevelMixin.java
@@ -1,11 +1,32 @@
 package org.boberchik342.CreateStormday.mixin;
 
+import com.mojang.logging.LogUtils;
+import dev.ryanhcode.sable.api.physics.handle.RigidBodyHandle;
 import dev.ryanhcode.sable.sublevel.ServerSubLevel;
 import org.joml.Vector3d;
+import org.joml.Vector3dc;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ServerSubLevel.class)
-public class ServerSubLevelMixin {
-    public Vector3d lastWakeUpForce = new Vector3d();
-    public Vector3d lastWakeUpTorque = new Vector3d();
+public abstract class ServerSubLevelMixin {
+    @Unique
+    public Vector3d createStormday$lastWakeUpForce = new Vector3d();
+    @Unique
+    public Vector3d createStormday$lastWakeUpTorque = new Vector3d();
+
+    @Redirect(method = "prePhysicsTick", at = @At(value = "INVOKE", target = "Ldev/ryanhcode/sable/api/physics/handle/RigidBodyHandle;applyLinearAndAngularImpulse(Lorg/joml/Vector3dc;Lorg/joml/Vector3dc;Z)V"))
+    void wakeUpper(RigidBodyHandle instance, Vector3dc impulse, Vector3dc torque, boolean wakeUp) {
+        boolean forceChanged = impulse.distanceSquared(createStormday$lastWakeUpForce) > 1e-5;
+        boolean torqueChanged = torque.distanceSquared(createStormday$lastWakeUpTorque) > 1e-5;
+        wakeUp |= forceChanged || torqueChanged;
+        if (wakeUp) {
+            createStormday$lastWakeUpForce.set(impulse);
+            createStormday$lastWakeUpTorque.set(torque);
+            LogUtils.getLogger().info("Woken Up Object");
+        }
+        instance.applyLinearAndAngularImpulse(impulse, torque, wakeUp);
+    }
 }
