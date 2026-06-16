61719eb08bd7f525e7be3b21467fbed1a43c4c52
boberchik342 <Mikhail.Kamyshev.10@gmail.com>
2026-06-16 12:24:05 -0400

    crash fixed

--- src/main/java/org/boberchik342/CreateStormday/raycast/RaycastHelper.java
+++ src/main/java/org/boberchik342/CreateStormday/raycast/RaycastHelper.java
@@ -7,13 +7,19 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 
 import java.util.List;
+import java.util.Map;
 import java.util.WeakHashMap;
 
 public class RaycastHelper {
-    private static final WeakHashMap<Level, RaycastOctree> octrees = new WeakHashMap<>();
+    private static final Map<Level, RaycastOctree> clientOctrees = new WeakHashMap<>();
+    private static final Map<Level, RaycastOctree> serverOctrees = new WeakHashMap<>();
 
     public static RaycastOctree getOctree(Level level) {
-        return octrees.computeIfAbsent(level, k -> new RaycastOctree(level));
+        return getOctreeMap(level.isClientSide).computeIfAbsent(level, k -> new RaycastOctree(level));
+    }
+
+    private static Map<Level, RaycastOctree>  getOctreeMap(boolean isClient) {
+        return isClient ? clientOctrees : serverOctrees;
     }
 
     public static boolean raycast(Level level, Vec3 pos, Vec3 direction) {
@@ -25,20 +31,18 @@ public class RaycastHelper {
     public static void tick(boolean isClient) {
         LogUtils.getLogger().info(String.valueOf(chunksLoaded));
         chunksLoaded = 0;
-        for (var entry : octrees.entrySet()) {
+        for (var entry : getOctreeMap(isClient).entrySet()) {
             Player player = null;
-            if (entry.getKey().isClientSide() == isClient) {
-                Vec3 pos;
-                if (isClient) {
-                    player = Minecraft.getInstance().player;
-                } else {
-                    List<Player> players = (List<Player>) entry.getKey().players();
-                    if (!players.isEmpty()) player = players.get(entry.getKey().random.nextInt(players.size()));
-                }
-                if (player != null) {
-                    pos = player.getEyePosition();
-                    entry.getValue().loadChunkNear(pos);
-                }
+            Vec3 pos;
+            if (isClient) {
+                player = Minecraft.getInstance().player;
+            } else {
+                List<Player> players = (List<Player>) entry.getKey().players();
+                if (!players.isEmpty()) player = players.get(entry.getKey().random.nextInt(players.size()));
+            }
+            if (player != null) {
+                pos = player.getEyePosition();
+                entry.getValue().loadChunkNear(pos);
             }
         }
     }
